f1ad43442a8550ecc3f2b2627582db34e583af72
tifs73 <c14180073@john.petra.ac.id>
2021-03-07 22:56:41 +0700

    3 files modified, 2 added

--- src/app/global-var.service.ts
+++ src/app/global-var.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class GlobalVarService {
-  private datajudul:String;
-  private dataisi:String;
-  private datatgl:String;
+  private datajudul: String;
+  private dataisi: String;
+  private datatgl: String;
 
-  constructor() { }
+  constructor() {}
 
+  public setJudul(judul: string) {
+    this.datajudul = judul;
+  }
+
+  public getJudul() {
+    return this.datajudul;
+  }
+
+  public setIsi(isi: string) {
+    this.dataisi = isi;
+  }
+
+  public getIsi() {
+    return this.dataisi;
+  }
+
+  public setTgl(tgl: string) {
+    this.datatgl = tgl;
+  }
+
+  public getTgl() {
+    return this.datatgl;
+  }
 }
